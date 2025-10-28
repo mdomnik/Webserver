0fd7c72f6aa37766c6b3cd2d952ce84c70e09a53
Maciej Domnik <mdomnik.maciej@gmail.com>
2025-10-28 15:19:27 +0100

    wokr on servermanager

---
+++ Server/inc/Server.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 14:13:26 by mdomnik           #+#    #+#             */
+/*   Updated: 2025/10/28 15:09:07 by mdomnik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <stdexcept>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "../../Config/inc/ServerConfig.hpp"
+
+class Server
+{
+	private:
+		int		_socketFD;
+		struct sockaddr_in	_serverAddr;
+		std::vector<int>	_clientSockets;
+		ServerConfig		_config;
+
+		// Server setup methods
+		void CreateSocket();
+		void BindandListen();
+
+	public:
+		// Constructor and Destructor
+		Server(const ServerConfig& config);
+		~Server();
+
+		// Server operations
+		void Start();
+		void Stop();
+		
+		// Getters
+		int GetSocketFD() const;
+		const ServerConfig& GetServerConfig() const;
+
+		// Client handling
+		int acceptClient();
+};
+
+#endif
---
+++ Server/src/Server.cpp
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 14:13:53 by mdomnik           #+#    #+#             */
+/*   Updated: 2025/10/28 15:18:51 by mdomnik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Server.hpp"
+
+// ==== Contructor and Destructor ====
+
+// Constructor
+Server::Server(const ServerConfig& config) : _socketFD(-1), _config(config)
+{
+	std::memset(&_serverAddr, 0, sizeof(_serverAddr));
+	_clientSockets.reserve(100); // reserve space for 100 clients
+}
+
+// Destructor
+Server::~Server()
+{
+	Stop();
+}
+
+// ==== Private Server Operations ====
+
+// Creates the server socket
+void Server::CreateSocket()
+{
+	_socketFD = socket(AF_INET, SOCK_STREAM, 0); // get TCP socket
+	if (_socketFD < 0)
+		throw std::runtime_error("Failed to create socket");
+	
+	int optionFlags = 1;
+	if (setsockopt(_socketFD, SOL_SOCKET, SO_REUSEADDR, &optionFlags, sizeof(optionFlags)) < 0) // set socket options that allow reuse of addr/port
+	{
+		close(_socketFD);
+		throw std::runtime_error("Failed to set socket options");
+	}
+
+	int flags = fcntl(_socketFD, F_GETFL, 0);
+	if (flags < 0 || fcntl(_socketFD, F_SETFL, flags | O_NONBLOCK) < 0) // set socket to non-blocking
+	{
+		close(_socketFD);
+		throw std::runtime_error("Failed to set socket to non-blocking");
+	}
+}
+
+// Binds and listens on the server socket
+void Server::BindandListen()
+{
+	// Bind the socket to the specified host and port
+	_serverAddr.sin_family = AF_INET;
+	_serverAddr.sin_port = htons(_config.port);
+	_serverAddr.sin_addr.s_addr = inet_addr(_config.host.c_str());
+
+	if (_serverAddr.sin_addr.s_addr == INADDR_NONE) // if invalid IP address
+	{
+		close(_socketFD);
+		throw std::runtime_error("Invalid IP address: " + _config.host);
+	}
+
+	if (bind(_socketFD, (struct sockaddr*)&_serverAddr, sizeof(_serverAddr)) < 0) // bind socket
+	{
+		close(_socketFD);
+		throw std::runtime_error("Failed to bind socket to address");
+	}
+
+	if (listen(_socketFD, SOMAXCONN) < 0) // start listening on socket
+	{
+		close(_socketFD);
+		throw std::runtime_error("Failed to listen on socket");
+	}
+}
+
+// ==== Public Server Operations ====
+
+// Starts the server and begins listening for connections
+void Server::Start()
+{
+	CreateSocket();
+	BindandListen();
+	std::cout << "Server started! Listening on " << _config.host << ":" << _config.port << " !" << std::endl;
+}
+
+// Stops the server and closes all connections
+void Server::Stop()
+{
+	for (size_t i = 0; i < _clientSockets.size(); ++i)
+		close(_clientSockets[i]);
+	
+	_clientSockets.clear();
+
+	if (_socketFD != -1)
+	{
+		close(_socketFD);
+		_socketFD = -1;
+		std::cout << "Server stopped running; All Clients disconnected." << std::endl;
+	}
+}
+
+// ==== Getters ====
+
+int Server::GetSocketFD() const
+{
+	return (_socketFD);
+}
+
+const ServerConfig& Server::GetServerConfig() const
+{
+	return (_config);
+}
+
+// ==== Client Handling ====
+
+int Server::acceptClient()
+{
+	struct
+}
+
+
+
